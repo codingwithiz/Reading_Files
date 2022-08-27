6ac8a6f8b9002fbbe9cb0efe543dd42fa6e823f4
Ing Zhen Lee <ingzhen2003@gmail.com>
2022-08-27 14:13:58 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+
+employee_file = open("Amelia Task 1.txt" , "r+")
+'''
+r: read
+r+ : read and write
+'''
+
+print(employee_file.read())
+'''
+print(employee_file.read()) - start to read
+print(employee_file.readline()) - read first line
+print(employee_file. readlines()) - read every lines in array
+'''
+
+employee_file.close
+
